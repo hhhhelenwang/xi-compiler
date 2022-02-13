5169c11737bb655f50fcc1c4e2969b025df63883
bz239 <bz239@cornell.edu>
2022-02-13 10:57:15 -0500

    add interface class

---
+++ src/main/java/jw795/ast/Interface.java
@@ -0,0 +1,12 @@
+package jw795.ast;
+import java.util.List;
+
+/**
+ * Representation of the program in AST. The root node of the AST.
+ * Contains a nonempty set of procedure and function declarations without implementations
+ * and may contain end-of-line comments.
+ */
+public class Interface implements ASTNode{
+
+}
+
