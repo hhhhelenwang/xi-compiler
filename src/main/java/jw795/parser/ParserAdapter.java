db46b4df07bb6c06bb280d3d79da6eda0082e82b
jw795 <jw795@cornell.edu>
2022-02-15 00:25:00 -0500

    parser adapater

--- src/main/java/jw795/parser/ParserAdapter.java
+++ src/main/java/jw795/parser/ParserAdapter.java
@@ -1,6 +1,16 @@
 package jw795.parser;
 
-public class ParserAdapter {
-//    Parser parser
+import jw795.lexer.Lexer;
 
+import java.io.Reader;
+import java_cup.runtime.Scanner;
+
+public class ParserAdapter {
+    Parser parser;
+    Scanner scanner;
+
+    ParserAdapter(Reader reader) {
+        scanner = new Lexer(reader);
+        parser = new Parser(scanner);
+    }
 }
