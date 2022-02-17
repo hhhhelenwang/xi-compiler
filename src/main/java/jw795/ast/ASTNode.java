4ff520d0a4552c5768978ca59ddfef9f12023ebd
archerluo <luogaochen@gmail.com>
2022-02-17 02:49:47 -0500

    add some line and column number. partly rearrange grammar into DFS order

--- src/main/java/jw795/ast/ASTNode.java
+++ src/main/java/jw795/ast/ASTNode.java
@@ -6,8 +6,8 @@ import util.edu.cornell.cs.cs4120.util.CodeWriterSExpPrinter;
  * Representation of an AST node. Stores node content.
  */
 public abstract class ASTNode implements Node{
-    int line;
-    int column;
+    public int line;
+    public int column;
 
     public ASTNode (int li, int co) {
         line = li;
