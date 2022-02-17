789b2ced2f5a5325f2701f076bd360510aec839a
jw795 <jw795@cornell.edu>
2022-02-17 02:18:22 -0500

    stringlit and add line and col

---
+++ src/main/java/jw795/ast/StringLit.java
@@ -0,0 +1,17 @@
+package jw795.ast;
+
+import util.edu.cornell.cs.cs4120.util.CodeWriterSExpPrinter;
+
+public class StringLit extends Expr{
+    String str;
+
+    StringLit(String s, int li, int co) {
+        super(li, co);
+        str = s;
+    }
+
+    @Override
+    public void prettyPrint(CodeWriterSExpPrinter printer) {
+        printer.printAtom(str);
+    }
+}
