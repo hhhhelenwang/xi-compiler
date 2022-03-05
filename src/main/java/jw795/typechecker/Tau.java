252afa9578781fee91d9f7bbbde52e859551352d
jw795 <jw795@cornell.edu>
2022-03-04 22:49:52 -0500

    fix function equals

--- src/main/java/jw795/typechecker/Tau.java
+++ src/main/java/jw795/typechecker/Tau.java
@@ -2,19 +2,24 @@ package jw795.typechecker;
 
 public class Tau implements T{
 
-    public boolean equals (Tau type) {
-        if (!(this instanceof Array)) {
-            if ((this instanceof Int && type instanceof Int) || (this instanceof Bool && type instanceof Bool)){
-                return true;
+    @Override
+    public boolean equals(T type) {
+        if (type instanceof Tau) {
+            if (!(this instanceof Array)) {
+                if ((this instanceof Int && type instanceof Int) || (this instanceof Bool && type instanceof Bool)){
+                    return true;
+                } else {
+                    return false;
+                }
             } else {
-                return false;
+                if (!(type instanceof Array)) {
+                    return false;   // first type is an array, second type is not an array
+                } else {
+                    return ((Array)this).compare((Array)type) || ((Array)type).compare((Array)this);
+                }
             }
         } else {
-            if (!(type instanceof Array)) {
-                return false;   // first type is an array, second type is not an array
-            } else {
-                return ((Array)this).compare((Array)type) || ((Array)type).compare((Array)this);
-            }
+            return false;
         }
     }
 
