0ca355c0780ceb334324ba9668f7629728d7310c
jw795 <jw795@cornell.edu>
2022-03-21 20:26:09 -0400

    a reorder framework

--- src/main/java/jw795/irgenerator/JumpReorder.java
+++ src/main/java/jw795/irgenerator/JumpReorder.java
@@ -1,7 +1,7 @@
 package jw795.irgenerator;
 
 import edu.cornell.cs.cs4120.xic.ir.IRCompUnit;
-import edu.cornell.cs.cs4120.xic.ir.IRNode;
+import edu.cornell.cs.cs4120.xic.ir.IRSeq;
 import edu.cornell.cs.cs4120.xic.ir.IRStmt;
 
 import java.util.ArrayList;
@@ -41,7 +41,57 @@ public class JumpReorder {
 
     }
 
-    public BasicBlock buildCFG(IRNode ir){
+    /**
+     * Reorder the given IR. Call this function to reorder a lowered IR.
+     * @param node IR
+     * @return reordered IR
+     */
+    public IRCompUnit reorder(IRCompUnit node) {
+        // TODO: go through all functions in node, build cfg for each function body, and generate a new
+        //  IRCompUnit from the new reordered function body
         return null;
     }
+
+    /**
+     * Construct a CFG from the given IRSeq node
+     * @param ir sequence
+     * @return cfg
+     */
+    public BasicBlock buildCFG(IRSeq ir){
+        //TODO: call getBasicBlocks and connectBlocks
+        return null;
+    }
+
+    /**
+     * Break an IR Sequence into a list of basic blocks.
+     * @return a list of basic blocks
+     */
+    public List<BasicBlock> getBasicBlocks(IRSeq node) {
+        return null;
+    }
+
+    /**
+     * Connect the basic blocks in blocks according to their control flow.
+     * @param blocks a list of basic blocks
+     * @return the root node for the connected CFG
+     */
+    public BasicBlock connectBlocks(List<BasicBlock> blocks) {
+        // TODO
+        for (BasicBlock block: blocks) {
+
+        }
+
+        return null;
+    }
+
+    /**
+     *  Greedily construct a trace from the CFG.
+     * @param root the root of a CFG
+     * @return the trace
+     */
+    public List<BasicBlock> buildTrace(BasicBlock root) {
+        //TODO
+        return null;
+    }
+
 }
