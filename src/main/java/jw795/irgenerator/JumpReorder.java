fda45b0e73f109c1f542da43e46df1412aa5f61d
bz239 <bz239@cornell.edu>
2022-03-22 23:55:22 -0400

    debug

--- src/main/java/jw795/irgenerator/JumpReorder.java
+++ src/main/java/jw795/irgenerator/JumpReorder.java
@@ -1,5 +1,6 @@
 package jw795.irgenerator;
 
+import edu.cornell.cs.cs4120.util.SExpPrinter;
 import edu.cornell.cs.cs4120.xic.ir.*;
 
 import java.util.*;
@@ -51,6 +52,14 @@ public class JumpReorder {
         this.basicBlocksMap = new HashMap<>();
     }
 
+    public void printSeq(IRSeq seq) {
+        List<IRStmt> stmts = seq.stmts();
+        for (IRStmt stmt : stmts) {
+            System.out.println(stmt.toString()); // Added here for debugging purpose
+//            stmt.printSExp(p);
+        }
+    }
+
     /**
      * Reorder the given IR. Call this function to reorder a lowered IR.
      * @param node IR
@@ -63,9 +72,13 @@ public class JumpReorder {
         for (IRFuncDecl function : node.functions().values()){
             IRStmt body = function.body();
             if ( body instanceof IRSeq){
+//                printSeq((IRSeq)body);
                 basicBlocksMap = new HashMap<>();
                 BasicBlock root = buildCFG(((IRSeq)function.body()));
                 List<BasicBlock> trace = buildTrace(root);
+
+                System.out.println(trace.size());
+
                 body = fixJumps(trace);
             }
             reorderedFunDecl.put(function.name(), irFactory.IRFuncDecl(function.name(), body));
@@ -83,9 +96,13 @@ public class JumpReorder {
         List<IRStmt> stmts = new ArrayList<>();
         BasicBlock curBlock;
         BasicBlock nxtBlock;
+        boolean prevIsCjump = false;
+        System.out.println("total number of blocks " + trace.size());
 
         // only need to worry about fixing jumps if there exist a next block
         for (int i = 0; i < trace.size()-1; i++){
+            System.out.println("enter block " + i);
+            System.out.println("block size " + trace.get(i).statements.size());
             curBlock = trace.get(i);
             nxtBlock = trace.get(i+1);
 
@@ -106,29 +123,43 @@ public class JumpReorder {
                         newCjump = new IRCJump(((IRCJump) curEndingStmt).cond(), ((IRCJump) curEndingStmt).trueLabel());
                     }
 
+                    prevIsCjump = true;
                     // update block with correct ending stmt
                     curBlock.statements.remove(curBlock.statements.size()-1); //remove last stmt
+//                    System.out.println();
                     stmts.addAll(curBlock.statements);
                     stmts.add(newCjump);
                 } else if (curEndingStmt instanceof IRJump){
+
                     //check if the next basic block has same label, if so, remove the jump
                     String nxtLabel = ((IRLabel) nxtBlock.statements.get(0)).name();
                     if (nxtLabel.equals(((IRJump) curEndingStmt).target())){
                         curBlock.statements.remove((curBlock.statements.size()-1));
                         stmts.addAll(curBlock.statements);
                     }
-                }
-                // do nothing for IRReturn
-            } else {
-                // check if we need to add JUMP if does not transfer control && not followed by next label
-                String nxtLabel = ((IRLabel) nxtBlock.statements.get(0)).name();
-                String expectedNxt = curBlock.nextBlockLabel.get();
-                if (!nxtLabel.equals(expectedNxt)){
-                    curBlock.statements.add(irFactory.IRJump(irFactory.IRName(expectedNxt)));
+
+
+                } else {
                     stmts.addAll(curBlock.statements);
                 }
+
+                // do nothing for IRReturn
+            } else {
+                if (prevIsCjump){
+                    curBlock.statements.remove(0); //remove first stmt (LABEL)
+                } else {
+                    // check if we need to add JUMP if does not transfer control && not followed by next label
+                    String nxtLabel = ((IRLabel) nxtBlock.statements.get(0)).name();
+                    String expectedNxt = curBlock.nextBlockLabel.get();
+                    if (!nxtLabel.equals(expectedNxt)) {
+                        curBlock.statements.add(irFactory.IRJump(irFactory.IRName(expectedNxt)));
+                    }
+                }
+                stmts.addAll(curBlock.statements);
             }
         }
+
+        stmts.addAll(trace.get(trace.size()-1).statements);
 
         return new IRSeq(stmts);
     }
