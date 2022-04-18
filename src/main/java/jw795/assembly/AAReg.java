68f445b020d4d7498ec4f039d58c713126d91f27
Tianjing Zhang <tz346@cornell.edu>
2022-04-18 02:13:13 -0400

    make all tmp an address

--- src/main/java/jw795/assembly/AAReg.java
+++ src/main/java/jw795/assembly/AAReg.java
@@ -5,7 +5,8 @@ package jw795.assembly;
  */
 public class AAReg extends AAOperand{
     String regName;
-
+    //caller-saved rax, rcx, rdx
+    //callee-saved rdi, rsi, rbx, rbp, r12-r15
     public AAReg(String name) {
         regName = name;
     }
