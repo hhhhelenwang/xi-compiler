95051c1f06359ff57553b8a09475622e4a511653
jw795 <jw795@cornell.edu>
2022-05-09 23:58:52 -0400

    ConditionalConstantPropagation class created but still figuring out how to do this

--- src/main/java/jw795/dfa/ConditionalConstantPropagation.java
+++ src/main/java/jw795/dfa/ConditionalConstantPropagation.java
@@ -1,4 +1,42 @@
 package jw795.dfa;
 
-public class ConditionalConstantPropagation {
+import jw795.assembly.AAInstruction;
+import jw795.cfg.CFG;
+import jw795.cfg.CFGNode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+// the lattice:
+// a map from variable names to its possible values
+// done on AA
+public class ConditionalConstantPropagation extends DataFlowAnalysis<HashMap<String, HashSet<Integer>>, AAInstruction> {
+    public ConditionalConstantPropagation(CFG<AAInstruction> cfg) {
+        super(cfg);
+    }
+
+    @Override
+    public HashMap<String, HashSet<Integer>> meet(List<HashMap<String, HashSet<Integer>>> input) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, HashSet<Integer>> fn(HashMap<String, HashSet<Integer>> l,
+                                                HashMap<String, HashSet<Integer>> gen,
+                                                HashMap<String, HashSet<Integer>> kill) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, HashSet<Integer>> gen(CFGNode<AAInstruction> node,
+                                                 HashMap<String, HashSet<Integer>> l) {
+        return null;
+    }
+
+    @Override
+    public HashMap<String, HashSet<Integer>> kill(CFGNode<AAInstruction> node,
+                                                  HashMap<String, HashSet<Integer>> l) {
+        return null;
+    }
 }
