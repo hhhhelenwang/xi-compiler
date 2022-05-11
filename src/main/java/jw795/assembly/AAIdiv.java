4a091a78cb0d98df6065fcd99459096d188006e1
gl355 <gl355@cornell.edu>
2022-05-11 03:56:50 -0400

    update live var gen adn some AAInst use

--- src/main/java/jw795/assembly/AAIdiv.java
+++ src/main/java/jw795/assembly/AAIdiv.java
@@ -1,9 +1,21 @@
 package jw795.assembly;
 
+import jw795.asmgenerator.Tiler;
+
+import java.util.HashSet;
+
 public class AAIdiv extends AAInstruction{
 
     public AAIdiv(AAOperand opr1) {
         super(opr1);
+    }
+
+    @Override
+    public HashSet<AAOperand> use() {
+        HashSet<AAOperand> useset = super.use();
+        useset.add(Tiler.rax);
+        useset.add(Tiler.rdx);
+        return useset;
     }
 
     @Override
--- src/main/java/jw795/assembly/AAMove.java
+++ src/main/java/jw795/assembly/AAMove.java
@@ -1,9 +1,21 @@
 package jw795.assembly;
 
+import java.util.HashSet;
+
 public class AAMove extends AAInstruction {
 
     public AAMove(AAOperand opr1, AAOperand opr2) {
         super(opr1, opr2);
+    }
+
+    @Override
+    public HashSet<AAOperand> use() {
+        HashSet<AAOperand> result = new HashSet<>();
+        if (operand1.get() instanceof AAMem) {
+            result.addAll(vars(operand1.get()));
+        }
+        result.addAll(vars(operand2.get()));
+        return result;
     }
 
     @Override
