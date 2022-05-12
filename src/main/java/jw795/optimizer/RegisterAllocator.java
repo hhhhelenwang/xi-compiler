96faa44d8c96c6e3c0a5cd0da4487ac05770d3ba
bz239 <bz239@cornell.edu>
2022-05-12 12:01:42 -0400

    regAlloc helpers

--- src/main/java/jw795/optimizer/RegisterAllocator.java
+++ src/main/java/jw795/optimizer/RegisterAllocator.java
@@ -1,16 +1,15 @@
 package jw795.optimizer;
 
-import jw795.assembly.AAInstruction;
-import jw795.assembly.AAMove;
-import jw795.assembly.AAOperand;
-import jw795.assembly.AATemp;
+import jw795.assembly.*;
 import jw795.cfg.AsmCFG;
 import jw795.cfg.CFGGenerator;
 import jw795.cfg.CFGNode;
 import jw795.dfa.LiveVariableAnalysis;
-import polyglot.util.Pair;
 
+import java.awt.*;
 import java.util.*;
+import java.util.List;
+import java.util.Set;
 
 public class RegisterAllocator {
     List<AAInstruction> instructionList;
@@ -36,11 +35,13 @@ public class RegisterAllocator {
     HashSet<GraphEdge> adjSet;
     HashMap<GraphNode, HashSet<GraphNode>> adjList;
     HashMap<GraphNode, Long> degree;
-    HashMap<GraphNode, AAMove> moveList;
+    HashMap<GraphNode, HashSet<AAMove>> moveList;
     HashMap<GraphNode, GraphNode> alias;
 
-    List<String> regs = Arrays.asList("rax", "rbx", "rcx", "rdx", "r8", "r9", "r10", "r11", "r12",
+
+    List<String> usableRegs = Arrays.asList("rax", "rbx", "rcx", "rdx", "r8", "r9", "r10", "r11", "r12",
             "r13", "r14", "r15","rsi","rdi","rsp","rbp");
+    int K = 16; //number of usable registers
 
     public RegisterAllocator(List<AAInstruction> instructionList) {
         this.instructionList = instructionList;
@@ -100,10 +101,6 @@ public class RegisterAllocator {
     public void build() {
     }
 
-    public void makeWorklist() {
-
-    }
-
     private void addEdge(AATemp u, AATemp v){
         //TODO: get uNode and vNode from interference graph, null init is temp
         GraphNode uNode = null;
@@ -123,4 +120,101 @@ public class RegisterAllocator {
             }
         }
     }
+
+    private HashSet<GraphNode> adjacent(GraphNode n){
+        HashSet adjSet = new HashSet(adjList.get(n));
+        HashSet union = new HashSet(selectStack);
+        union.addAll(coalescedNodes);
+        adjSet.removeAll(union);
+        return adjSet;
+    }
+
+    private void makeWorklist() {
+        for (GraphNode n : initial){
+            initial.remove(n);
+            if (degree.get(n) >= K){
+                spillWorklist.add(n);
+            } else if (moveRelated(n)){
+                freezeWorklist.add(n);
+            } else {
+                simplifyWorklist.add(n);
+            }
+        }
+    }
+
+    private boolean moveRelated(GraphNode n) {
+        return nodeMoves(n).size() != 0;
+    }
+
+    private HashSet<AAMove> nodeMoves(GraphNode n) {
+        HashSet<AAMove> activeM = new HashSet<>(activeMoves);
+        HashSet<AAMove> worklistM = new HashSet<>(worklistMoves);
+        HashSet<AAMove> moveLst = new HashSet<>(moveList.get(n));
+
+        activeM.addAll(worklistM);
+        moveLst.removeAll(activeM);
+
+        return moveLst;
+    }
+
+    private void simplify(){
+        for (GraphNode n : simplifyWorklist){
+            simplifyWorklist.remove(n);
+            selectStack.add(n);
+
+            for (GraphNode m : adjacent(n)){
+                decrementDegree(m);
+            }
+        }
+    }
+
+    private void decrementDegree(GraphNode m) {
+        Long d = degree.get(m);
+        degree.put(m, degree.get(m)-1);
+        if (d == K){
+            HashSet adjNodes = adjacent(m);
+            adjNodes.add(m);
+            enableMoves(adjNodes);
+            spillWorklist.remove(m);
+            if (moveRelated(m)){
+                freezeWorklist.add(m);
+            } else {
+                simplifyWorklist.add(m);
+            }
+        }
+    }
+
+    private void enableMoves(HashSet<GraphNode> nodes) {
+        for (GraphNode n : nodes){
+            for (AAMove m : nodeMoves(n)){
+                if (activeMoves.contains(m)){
+                    activeMoves.remove(m);
+                    worklistMoves.add(m);
+                }
+            }
+        }
+    }
+
+    private void addWorkList(GraphNode u){
+        if (!precolored.contains(u) && !(moveRelated(u) && degree.get(u) < K)){
+            freezeWorklist.remove(u);
+            simplifyWorklist.add(u);
+        }
+    }
+
+    private boolean ok(GraphNode t, GraphNode r){
+        // TODO: double check adjSet.contains(new GraphEdge) would work: compare by value
+        return degree.get(r) < K && precolored.contains(t) && adjSet.contains(new GraphEdge(t, r));
+    }
+
+    private boolean conservative(HashSet<GraphNode> nodes){
+        int k = 0;
+        for (GraphNode n : nodes){
+            if (degree.get(n) >= K){
+                k++;
+            }
+        }
+        return k<K;
+    }
+
 }
